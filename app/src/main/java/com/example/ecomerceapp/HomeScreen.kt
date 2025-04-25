30e618f6bf504ab57101eaf13f70ae69e89034a6
Efrain Alvarez <ealvarez169@unab.edu.co>
2025-04-24 19:43:22 -0500

    cambio iconos

--- app/src/main/java/com/example/ecomerceapp/HomeScreen.kt
+++ app/src/main/java/com/example/ecomerceapp/HomeScreen.kt
@@ -1,25 +1,122 @@
 package com.example.ecomerceapp
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil3.compose.rememberAsyncImagePainter
 import com.example.ecomerceapp.ui.theme.EcomerceAppTheme
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(navcontroller: NavController) {
-    Scaffold { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text(text = "Home Screen", fontSize = 50.sp)
+fun HomeScreen(navcontroller: NavController?=null) {
+
+
+    Scaffold(
+        topBar = {
+            val scrollBehavior =
+                TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+            MediumTopAppBar(
+
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Medium Top App Bar",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ExitToApp,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        },
+    ) { innerPadding ->Column(modifier = Modifier.padding(innerPadding)) {
+        Text(text = "Promociones", modifier = Modifier.padding(top = 16.dp, bottom = 16.dp, start = 16.dp))
+        val listaurls=listOf(
+            "https://img.freepik.com/psd-premium/etiqueta-roja-super-oferta_658787-247.jpg",
+            "https://img.freepik.com/vector-gratis/ilustracion-etiqueta-venta-papel-color-rojo-sombra_91128-760.jpg?semt=ais_hybrid&w=740",
+            "https://www.clipartmax.com/png/middle/258-2588815_promocion-especial-png.png",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ83sh3XIQzG7R0CkVtZMxq1VMSymkeCPpipw&s",
+            "https://static.vecteezy.com/system/resources/thumbnails/009/383/648/small_2x/hot-sale-emblem-clipart-design-illustration-free-png.png",
+
+        )
+        LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp), contentPadding = PaddingValues(horizontal = 16.dp)) {
+            for (url in listaurls){
+                item { CardPromo(url) }
+            }
+
         }
+
     }
+    }
+}
+
+
+@Composable
+fun CardPromo(url: String) {
+    Card(
+        modifier = Modifier
+            .height(180.dp)
+            .width(300.dp)
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(url),
+            contentDescription = "Promocion",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+    }
+
+
 }
 
 @Preview
@@ -28,6 +125,5 @@ fun PreviewHomeScreen(
 
 ) {
     EcomerceAppTheme {
-//        HomeScreen()
-    }
+        HomeScreen()}
 }
